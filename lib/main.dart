f7f963c9d9b5b26fe1de28282cd3f1fa6d85d23e
heathermortensen <mortensenheather@hotmail.com>
2020-08-15 08:56:06 -0700

    changed app to a more neutral grey, instead of green

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,14 @@ class MyMuseumApp extends StatelessWidget {
     //Scaffold has a white background and allows us to add an appBar
     return MaterialApp(home: Scaffold(
                                 appBar: AppBar(title: Text('MyMuseumApp', 
-                                                style: TextStyle(fontSize: 30)), 
-                                                backgroundColor: Colors.green[300]
+                                                style: TextStyle(fontSize: 30, color: Colors.grey[400])), 
+                                                //backgroundColor: Colors.green[300]
+                                                //backgroundColor: Colors.brown[600]
+                                                backgroundColor: Colors.grey[900]
                                               ),
-                       backgroundColor: Colors.green[100],
+                       //backgroundColor: Colors.green[100],
+                       //backgroundColor: Colors.brown[100],
+                       backgroundColor: Colors.grey[500],
                        body: 
                                 Column(
                                         //An array/List of generic type's <Widget>
@@ -45,7 +49,7 @@ class MyMuseumApp extends StatelessWidget {
                                                             //           //shape: BoxShape.circle
                                                             //         ),
                                                                     
-                                                            Container(child: Text("Write something here"), color: Colors.blue, margin: EdgeInsets.all(10.0), padding: EdgeInsets.all(20.0)),
+                                                            Container(child: Text("Visit The Museum of Russian Art (Link Here)", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold ,color: Colors.yellow[200])), color: Colors.grey[900], width: double.infinity, /*margin: EdgeInsets.all(10.0),*/ padding: EdgeInsets.all(20.0)),
 
                                                             Text("Write something here",
                                                                   style: TextStyle(
